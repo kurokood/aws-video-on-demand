988e6fd56d45ba9fe544f984d4f71b3c3ec14ab2
kurokood <villarinmon@gmail.com>
2025-09-22 14:43:55 +0800

    updated terraform version and aws provider

--- IaC/main.tf
+++ IaC/main.tf
@@ -2,26 +2,30 @@
 # Main configuration file
 
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.12"
+      version = "~> 5.70"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1"
+      version = "~> 3.6"
     }
     null = {
       source  = "hashicorp/null"
       version = "~> 3.2"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
   }
-    backend "s3" {
+  backend "s3" {
     bucket         = "tf-state-store-121485"
     key            = "vod/terraform.tfstate"
     region         = "us-east-1"
-    use_lockfile   = true
+    dynamodb_table = "tf-state-lock-121485"
   }
 }
 
